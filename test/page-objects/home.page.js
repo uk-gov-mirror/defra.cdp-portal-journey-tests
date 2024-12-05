49fdd03487782ab1abe09522afc1ac343cd03652
Ivar Abrahamsen <ivar.abrahamsen@defra.gov.uk>
2024-12-05 13:41:47 +0000

    Basic home page migrated

--- test/page-objects/home.page.js
+++ test/page-objects/home.page.js
@@ -1,6 +1,24 @@
+import { $ } from '@wdio/globals'
+
 import { Page } from 'page-objects/page'
 
 class HomePage extends Page {
+  serviceName() {
+    return $('[data-testid="app-header-service-name"]')
+  }
+
+  userName() {
+    return $('[data-testid="app-login-username"]')
+  }
+
+  /**
+   * Check if the home nav link is active
+   * @returns {Promise<boolean>}
+   */
+  navIsActive() {
+    return super.navIsActive('home')
+  }
+
   open() {
     return super.open('/')
   }
--- test/page-objects/page.js
+++ test/page-objects/page.js
@@ -1,12 +1,50 @@
 import { browser, $ } from '@wdio/globals'
 
 class Page {
-  get pageHeading() {
+  pageHeading() {
     return $('h1')
+  }
+
+  navItem(navItemName) {
+    return $(`[data-testid="nav-${navItemName}"]`)
+  }
+
+  /**
+   * Check if a navigation item is active
+   * @param navItemName
+   * @returns {Promise<boolean>}
+   */
+  async navIsActive(navItemName) {
+    const navItem = await this.navItem(navItemName)
+    const className = await navItem.getAttribute('class')
+
+    return className.includes('app-navigation__link--active')
+  }
+
+  logInLink(value = 'Sign in') {
+    return $('[data-testid="app-login-link"]*=' + value)
+  }
+
+  logOutLink(value = 'Sign out') {
+    return $('[data-testid="app-login-link"]*=' + value)
+  }
+
+  async login() {
+    await this.open('/')
+    await this.logInLink().click()
+  }
+
+  async logOut() {
+    await this.open('/')
+    await this.logOutLink().click()
   }
 
   open(path) {
     return browser.url(path)
+  }
+
+  link(value) {
+    return $('a*=' + value)
   }
 }
 
