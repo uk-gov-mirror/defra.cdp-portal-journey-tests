54276027aaf61e75efab1c0b1ae6d09b459865a4
chotai <chotai@users.noreply.github.com>
2025-09-17 19:22:48 +0100

    CORE-1386 Add Portal DynamoDB table

---
+++ scripts/localstack/48-setup-dynamodb.sh
@@ -0,0 +1,5 @@
+aws --endpoint $LOCALSTACK_URL dynamodb create-table --region $AWS_REGION --table-name cdp-portal-frontend-session --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
+aws --endpoint $LOCALSTACK_URL dynamodb update-time-to-live --region $AWS_REGION --table-name cdp-portal-frontend-session --time-to-live-specification "Enabled=true,AttributeName=expiresAt"
+
+# aws --endpoint http://localhost:4566 dynamodb create-table --region eu-west-2 --table-name cdp-portal-frontend-session --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
+# aws --endpoint http://localhost:4566 dynamodb update-time-to-live --region eu-west-2 --table-name cdp-portal-frontend-session --time-to-live-specification "Enabled=true,AttributeName=expiresAt"
