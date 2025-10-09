c40c7ccebe41d0cc8b17fdfa1418efc2331669f1
chotai <chotai@users.noreply.github.com>
2025-10-09 18:05:26 +0100

    CORE-1388 Add cdp-uploader download requests queue

--- scripts/localstack/50-setup-s3-notification.sh
+++ scripts/localstack/50-setup-s3-notification.sh
@@ -4,17 +4,8 @@ echo Setting up Portal Buckets Notifications
 
 source /etc/localstack/conf.d/local-defaults.env
 
-aws --endpoint-url=$LOCALSTACK_URL s3api put-bucket-notification-configuration \
-    --bucket cdp-uploader-quarantine --region $AWS_REGION \
-    --notification-configuration '{
-                                      "QueueConfigurations": [
-                                         {
-                                           "QueueArn": "arn:aws:sqs:eu-west-2:000000000000:mock-clamav",
-                                           "Events": ["s3:ObjectCreated:*"]
-                                         }
-                                       ]
-	                                }'
-
+aws --endpoint-url=http://localhost:4566 s3api put-bucket-notification-configuration --bucket cdp-uploader-quarantine \
+    --notification-configuration '{"QueueConfigurations": [{"QueueArn": "arn:aws:sqs:eu-west-2:000000000000:mock-clamav","Events": ["s3:ObjectCreated:*"]}]}'
 
 echo Done!
 
